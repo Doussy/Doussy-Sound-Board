0268fb8492b3d029c4ace227bdef9fe12124993e
André Doussy <doussya@gmail.com>
2022-01-30 19:51:45 +0100

    Start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,89 @@
 import Head from 'next/head'
+import { useState } from 'react'
+
+let tabs = [
+  {
+    name: 'Animals',
+    items: [
+      {
+        name: 'Dog',
+        image: 'dog.jpg',
+        sound: 'dog.mp3',
+      },
+      {
+        name: 'Cat',
+        image: 'cat.jpg',
+        sound: 'cat.mp3',
+      },
+    ],
+  },
+  {
+    name: 'People',
+    items: [
+      {
+        name: 'Jan',
+        image: 'dog.jpg',
+        sound: 'dog.mp3',
+      },
+      {
+        name: 'Kobus',
+        image: 'cat.jpg',
+        sound: 'cat.mp3',
+      },
+    ],
+  },
+  {
+    name: 'Funny',
+    items: [
+      {
+        name: 'Fart',
+        image: 'dog.jpg',
+        sound: 'dog.mp3',
+      },
+      {
+        name: 'Fart2',
+        image: 'cat.jpg',
+        sound: 'cat.mp3',
+      },
+    ],
+  },
+]
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState('Animals')
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="text-center">
       <Head>
-        <title>Create Next App</title>
+        <title>Doussy Sound Board</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <div className="bg-gray-300 p-3 font-bold">
+        <div>Doussy Sound Board</div>
+        <div>Active Tab: {activeTab}</div>
+      </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+      <div className="flex justify-between">
+        {tabs.map((tab) => (
+          <div
+            className="cursor-pointer bg-amber-500 p-2 text-xl font-bold"
+            onClick={() => {
+              setActiveTab(tab.name)
+            }}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+            {tab.name}
+          </div>
+        ))}
+      </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
+      <div>
+        {tabs
+          .filter(item => item.name === activeTab)[0]
+          .items.map((item) => (
+            <div>{item.name}</div>
+          ))}
+      </div>
     </div>
   )
 }
